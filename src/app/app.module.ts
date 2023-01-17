574c394e370bcefd09c4fdea37c2a0cfd1e79d35
Max Carlquist <tenkaklet@gmail.com>
2023-01-17 20:11:32 +0100

    Begun Work on Sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
