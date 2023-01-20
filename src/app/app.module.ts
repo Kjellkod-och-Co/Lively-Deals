03a27a2f3f886c64ddea0504689ba76fdb8e5e58
Max Carlquist <tenkaklet@gmail.com>
2023-01-20 16:06:05 +0100

    Feature/sidebar (#2)
    * Begun Work on Sidebar
    
    * Finished Sidebar & base structure
    
    Begun work on sidebar, some customization is needed. Also added the relevant folders and routes for the app to work.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './home/search/search.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ExploreComponent } from './home/explore/explore.component';
+import { LibraryComponent } from './home/library/library.component';
+import { ProfileComponent } from './home/profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SidebarComponent,
+    SearchComponent,
+    ExploreComponent,
+    LibraryComponent,
+    ProfileComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
