04368e478f87ef9e3d6e027058dc8cb25d98b568
Max Carlquist <tenkaklet@gmail.com>
2023-01-24 13:59:38 +0100

    Added custom green color to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "lean-green": "#093a05"
+      },
+    },
   },
   plugins: [require('rippleui')],
+  rippleui: {},
 }
